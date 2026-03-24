973b5ebf548d3f5c6a651f23f93b34ec493a0592
yangq <yangq@YANGQ-LT>
2026-03-24 00:51:18 -0400

    Add project files.

---
+++ DatabaseExample/Form1.cs
@@ -0,0 +1,34 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DatabaseExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;
+              AttachDbFilename=|DataDirectory|\Database1.mdf;
+              Integrated Security=True;
+              Connect Timeout=30;";
+
+            using var conn = new SqlConnection(cs);
+            conn.Open();
+
+            using var cmd = new SqlCommand("SELECT Name FROM [Table]", conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var name = reader.GetString(0);
+                listBox1.Items.Add(name);
+            }
+
+
+        }
+    }
+}
